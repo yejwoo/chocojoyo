46ee073a0acd9c7938c5a4b9497626079d6f9784
yejwoo <yejinwoo.me@gmail.com>
2025-01-20 14:30:22 +0900

    chore: 폰트, 컬러, 이미지, 아이콘 asset 세팅

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import localFont from 'next/font/local';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const myFont = localFont({
+  src: './meetme.ttf',
+  display: 'swap'
+})
 
 export const metadata = {
   title: "Create Next App",
@@ -19,9 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={myFont.className}>
         {children}
       </body>
     </html>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,8 +8,73 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        brand: {
+          100: "#FF6682",
+          200: "#3D2505",
+        },
+        chocolates: {
+          green: {
+            100: "#95C43D",
+            200: "#72A90B",
+          },
+          milk: {
+            100: "#894E00",
+            200: "#3D2505",
+          },
+          dark: {
+            100: "#533608",
+            200: "#3F2802",
+          },
+          ruby: {
+            100: "#FEACB1",
+            200: "#F77B83",
+          },
+          vanilla: {
+            100: "#FFF5BA",
+            200: "#DDD397",
+          },
+          red: {
+            100: "#E54932",
+            200: "#B52915",
+          },
+        },
+        grayscale: {
+          0: "#FFF",
+          50: "#D8D3BD",
+          100: "#C1BCB5",
+          200: "#90887D",
+          300: "#271B0B",
+        },
+        popup: {
+          100: "#FEF9E1",
+          200: "#F9EAA4",
+        },
+        boxes: {
+          purple: {
+            box: "#FAECBC",
+            ribbon: "#E6F4F1",
+          },
+          yellow: {
+            box: "#FFCABE",
+            ribbon: "#FAECBC",
+          },
+          beige: {
+            box: "#FEF4EA",
+            ribbon: "#B7A99A",
+          },
+          pink: {
+            box: "#FFB8D0",
+            ribbon: "#BDA5AD",
+          },
+          apricot: {
+            box: "#FFCABE",
+            ribbon: "#EDE9D0",
+          },
+          skyblue: {
+            box: "#DCFCF6",
+            ribbon: "#FFC5D9",
+          },
+        },
       },
     },
   },
