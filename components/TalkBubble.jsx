e1a70b69dc518e9f75dddf8474e1813f6caef52f
yejwoo <yejinwoo.me@gmail.com>
2025-02-03 23:30:03 +0900

    style: 말풍선 svg 이미지로 변경

--- components/TalkBubble.jsx
+++ components/TalkBubble.jsx
@@ -1,21 +1,22 @@
-import { talkBubbleBodySm, talkBubbleTail } from "@/public/images/common";
+// import { talkBubbleBodySm, talkBubbleTail } from "@/public/images/common";
+import { talkBubble } from "@/public/images/common";
 import Image from "next/image";
 
 export default function TalkBubble({ dialogue }) {
   return (
     <div id="talk-bubble">
-      <div className="absolute bottom-[420px] right-3 w-48">
-        <Image src={talkBubbleBodySm} alt="말풍선" />
+      <div className="absolute bottom-[400px] right-4 w-56">
+        <Image src={talkBubble} alt="말풍선" />
         <p
-          className="absolute left-4 top-1/2 -translate-y-1/2 text-2xl"
+          className="absolute left-12 top-1/2 -translate-y-1/2 text-2xl text-left"
           dangerouslySetInnerHTML={{ __html: dialogue }}
           onDragStart={(e) => e.preventDefault()}
           draggable={false}
         />
       </div>
-      <div className="absolute bottom-[428px] right-[196px] w-7">
+      {/* <div className="absolute bottom-[428px] right-[196px] w-7">
         <Image src={talkBubbleTail} alt="말풍선" />
-      </div>
+      </div> */}
     </div>
   );
 }
