de9e47c62e26a1182b7f5ea0e5a0802fa6a40a64
yejwoo <yejinwoo.me@gmail.com>
2025-02-04 20:16:31 +0900

    chore: stage5 이미지 경로 변경

--- public/images/stage5/index.js
+++ public/images/stage5/index.js
@@ -1,8 +1,8 @@
-export { default as toppingAlmond } from './topping-almond.svg';
-export { default as toppingBlueberries } from './topping-blueberries.svg';
-export { default as toppingCoffeeBean } from './topping-coffeebean.svg';
-export { default as toppingHeart } from './topping-heart.svg';
-export { default as toppingSprinkle } from './topping-sprinkle.svg';
-export { default as toppingStrawberry } from './topping-strawberry.svg';
+export { default as toppingAlmond } from './toppings/topping-almond.svg';
+export { default as toppingBlueberries } from './toppings/topping-blueberries.svg';
+export { default as toppingCoffeeBean } from './toppings/topping-coffeebean.svg';
+export { default as toppingHeart } from './toppings/topping-heart.svg';
+export { default as toppingSprinkle } from './toppings/topping-sprinkle.svg';
+export { default as toppingStrawberry } from './toppings/topping-strawberry.svg';
 export { default as magnifierDefault } from './magnifier-default.svg';
 export { default as magnifierActive } from './magnifier-active.svg';
