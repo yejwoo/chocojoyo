c35d4e036517dab4fbde9468b9c4eecad3081ada
yejwoo <yejinwoo.me@gmail.com>
2025-02-04 20:05:18 +0900

    style: stage5 - 돋보기 버튼 이미지 변경

--- components/BottomNavi.jsx
+++ components/BottomNavi.jsx
@@ -74,8 +74,8 @@ export const BottomNavi = ({ stage, selectionState, setSelectionState, uiState, 
             </div>
           ))}
           {/* ✅ 돋보기 버튼 */}
-          <button className="px-2 rounded-tl-xl rounded-tr-xl" onClick={() => handleZoomMode(setUIState)}>
-            <Image src={uiState.isZoomMode ? magnifierActive : magnifierDefault} alt="돋보기" />
+          <button className="px-1 rounded-tl-xl rounded-tr-xl" onClick={() => handleZoomMode(setUIState)}>
+            <Image width={40} height={40} src={uiState.isZoomMode ? magnifierActive : magnifierDefault} alt="돋보기" />
           </button>
         </div>
       )}
