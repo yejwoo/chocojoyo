d061427ce24f4b892d054abf3e330cd61bdb2582
yejwoo <yejinwoo.me@gmail.com>
2025-02-08 19:08:07 +0900

    style: 흰 테두리 안 보이도록 svg -> png로 변경

--- public/images/stage3/index.js
+++ public/images/stage3/index.js
@@ -1,5 +1,5 @@
 export { default as doubleBoiler } from './double-boiler.svg';
-export { default as spatula } from './spatula.svg';
+export { default as spatula } from './spatula.png';
 export { default as meltedChocolate1 } from './melted-chocolate1.svg';
 export { default as meltedChocolate2 } from './melted-chocolate2.svg';
 export { default as meltedChocolate3 } from './melted-chocolate3.svg';
