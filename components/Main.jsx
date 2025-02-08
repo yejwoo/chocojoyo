d9c9bae01ecb65510e8baa231cafe570fde380b3
yejwoo <yejinwoo.me@gmail.com>
2025-02-08 12:57:45 +0900

    style: 이미지 width, height 인라인으로 변경 & priority 속성 추가

--- components/Main.jsx
+++ components/Main.jsx
@@ -9,8 +9,8 @@ export default function Main({ onStart }) {
 
         {/* 구름 */}
         <div className="absolute w-full h-72 bottom-[10%] left-1/2 -translate-x-1/2">
-          <Image src={cloud1} width={200} height={160} className="absolute bottom-0 left-[-48px] animate-cloud-left-right" alt="구름" />
-          <Image src={cloud2} width={160} height={160} className="absolute right-[-48px] animate-cloud-left-right" alt="구름" />
+          <Image style={{width: "200px", height:"160px"}} src={cloud1} className="absolute bottom-0 left-[-48px] animate-cloud-left-right" alt="구름" />
+          <Image style={{width: "160px", height: "160px"}} src={cloud2} width={160} height={160} className="absolute right-[-48px] animate-cloud-left-right" alt="구름" />
         </div>
         {/* 하트 & 로고 */}
         <div className="flex flex-col justify-center items-center absolute top-[5%] max-h-sm:top-1 w-full h-64">
@@ -18,7 +18,7 @@ export default function Main({ onStart }) {
             {/* 하트 */}
             <Image src={heart} className="w-[240px] max-h-sm:w-[228px]" alt="하트" />
             {/* 로고 */}
-            <Image src={logo} className="absolute w-[324px] max-h-sm:w-[300px]" alt="초코조요" />
+            <Image priority src={logo} className="absolute w-[324px] max-h-sm:w-[300px]" alt="초코조요" />
           </div>
           {/* 슬로건 */}
           <p className="w-full text-center text-xl max-h-sm:hidden">나만의 초콜릿으로 달콤한 마음을 전해줘요</p>
@@ -26,11 +26,11 @@ export default function Main({ onStart }) {
         {/* 오브젝트들 */}
         <div className="absolute w-full h-56 bottom-[25%] max-h-sm:bottom-[20%]">
           {/* 초콜릿 오브젝트 */}
-          <Image src={choco1} width={44} height={44} className="absolute left-6 rotate-6 animate-choco-left-right" alt="초콜릿" />
-          <Image src={choco2} width={48} height={48} className="absolute right-4 bottom-1/2 -translate-y-1/2 animate-choco-right-left" alt="초콜릿" />
-          <Image src={choco3} width={40} height={40} className="absolute left-4 bottom-0 animate-choco-left-right" alt="초콜릿" />
+          <Image style={{width:"44", height: "44"}} src={choco1} className="absolute left-6 rotate-6 animate-choco-left-right" alt="초콜릿" />
+          <Image style={{width:"48", height: "48"}} src={choco2} className="absolute right-4 bottom-1/2 -translate-y-1/2 animate-choco-right-left" alt="초콜릿" />
+          <Image style={{width:"40", height: "40"}} src={choco3} className="absolute left-4 bottom-0 animate-choco-left-right" alt="초콜릿" />
           {/* 얀토 */}
-          <Image src={yanto} width={200} height={200} className="absolute -translate-x-1/2 left-1/2 animate-bounce-up-middle" alt="얀토" />
+          <Image style={{width: "200px", height: "auto"}} src={yanto} className="absolute -translate-x-1/2 left-1/2 animate-bounce-up-middle" alt="얀토" />
         </div>
         {/* <p className="absolute bottom-24 w-full text-center text-gray-warm-200 text-xl">벌써 0명이 만들었어요!</p> */}
         {/* 버튼 */}
