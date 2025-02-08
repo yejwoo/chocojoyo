426efc3088bd334b3b5709e70b4dc54f48dd1b4a
yejwoo <yejinwoo.me@gmail.com>
2025-02-08 21:16:10 +0900

    feat: 테스트 모드 플래그 추가

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,7 @@
 import tailwindConfig from "@/tailwind.config.mjs";
 
 export const DOMAIN = "chocojoyo.com"
+export const MAX_HISTORY_LENGTH = 50;
 export const chocolateColors = tailwindConfig.theme.extend.colors.chocolates;
 export const chocopenColors = tailwindConfig.theme.extend.colors.chocopens;
+export const isTestMode = true;
