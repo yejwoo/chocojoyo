47a059dbea540036d23878b7b9c904f9683a6186
yejwoo <yejinwoo.me@gmail.com>
2025-02-10 00:39:57 +0900

    feat: 카드 제출 후 데이터 받아오는 동안 로딩 렌더링

--- components/layout/CardLayout.jsx
+++ components/layout/CardLayout.jsx
@@ -7,6 +7,7 @@ import { supabase } from "@/lib/supabaseClient";
 import Container from "./Container";
 import { useRouter } from "next/navigation";
 import Chocolates from "../Chocolates";
+import CustomLoading from "../CustomLoading";
 
 const CardLayout = forwardRef(({ chocolateInfo, mode = "write", initialData, id, onOpen, onComplete }, ref) => {
   const cardRef = useRef(null);
@@ -16,6 +17,7 @@ const CardLayout = forwardRef(({ chocolateInfo, mode = "write", initialData, id,
 
   const [formData, setFormData] = useState(initialData || { message: "", name: "", shapes: [], toppings: [], drawings: [] });
   const [isCompleted, setIsCompleted] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const chocolateInfoRef = useRef(chocolateInfo);
   const MAX_LENTH = 60;
 
@@ -58,64 +60,84 @@ const CardLayout = forwardRef(({ chocolateInfo, mode = "write", initialData, id,
     getChocoElements: () => chocoRefs.current,
   }));
 
+  const handleSubmit = async () => {
+    if (isSubmitting) return; // 중복 제출 방지
+    setIsSubmitting(true);
+    await onComplete(formData);
+    setIsSubmitting(false);
+  };
+
   const showCompleteButton = mode === "write";
 
   return (
     <Container>
-      <div ref={cardRef} className="w-full h-full relative flex flex-col items-center justify-between">
-        <Image src={bgBrown} alt="배경화면" className="absolute bottom-0 choco-bg" draggable={false} />
-        <div className="absolute top-1/2 -translate-y-1/2 flex flex-col gap-3 w-[320px] max-h-sm:top-5 max-h-sm:translate-y-0">
-          <div className="shadow-lg bg-white rounded-lg">
-            <div className="w-full py-5">
-              <div ref={boxRef} className="relative z-10 w-[280px] h-[182px] flex justify-center items-center mx-auto">
-                <Image src={box} alt="초콜릿 틀" width={280} height={280} className="absolute bottom-0" draggable={false} />
-                <div className="w-full flex justify-center items-center flex-wrap gap-x-2 gap-y-2">
-                  <Chocolates chocolateInfo={chocolateInfo} showDrawings={true} chocoRefs={chocoRefs}/>
+      {isSubmitting ? (
+        <CustomLoading />
+      ) : (
+        <>
+          <div ref={cardRef} className="w-full h-full relative flex flex-col items-center justify-between">
+            <Image src={bgBrown} alt="배경화면" className="absolute bottom-0 choco-bg" draggable={false} />
+            <div className="absolute top-1/2 -translate-y-1/2 flex flex-col gap-3 w-[320px] max-h-sm:top-5 max-h-sm:translate-y-0">
+              <div className="shadow-lg bg-white rounded-lg">
+                <div className="w-full py-5">
+                  <div ref={boxRef} className="relative z-10 w-[280px] h-[182px] flex justify-center items-center mx-auto">
+                    <Image src={box} alt="초콜릿 틀" width={280} height={280} className="absolute bottom-0" draggable={false} />
+                    <div className="w-full flex justify-center items-center flex-wrap gap-x-2 gap-y-2">
+                      <Chocolates chocolateInfo={chocolateInfo} showDrawings={true} chocoRefs={chocoRefs} />
+                    </div>
+                  </div>
                 </div>
+
+                {mode === "write" ? (
+                  <div className="w-full flex flex-col justify-center items-center pb-4">
+                    <textarea
+                      name="message"
+                      value={formData.message}
+                      onChange={handleInputChange}
+                      className="w-[280px] mx-auto h-[108px] text-left text-2xl leading-9 flex"
+                      placeholder="편지는 세 줄까지 쓸 수 있어요."
+                      maxLength={MAX_LENTH}
+                    />
+                    <div className="mt-2 relative text-center border-b border-gray-warm-100">
+                      <span className="text-lg">From. </span>
+                      <input
+                        name="name"
+                        onChange={handleInputChange}
+                        className="text-left w-[130px]"
+                        type="text"
+                        placeholder="이름은 열 글자까지"
+                        maxLength={10}
+                      />
+                    </div>
+                  </div>
+                ) : (
+                  <div className="w-full flex flex-col justify-center items-center pb-4">
+                    <p className="w-[280px] mx-auto h-[108px] text-left text-2xl leading-9 flex whitespace-pre-line">{formData.message}</p>
+                    <div className="mt-2 relative text-center">
+                      <div className="absolute inset-0 pointer-events-none" />
+                      <span className="text-lg relative z-10">From. </span>
+                      <span className="relative z-10">{formData.name}</span>
+                    </div>
+                  </div>
+                )}
               </div>
+
+              {showCompleteButton ? (
+                <Button size="md" onClick={handleSubmit} disabled={!isCompleted} message="완성" />
+              ) : (
+                <div className="flex gap-2 justify-ceㅘnter no-capture">
+                  <Button size="half" color="main" message={"공유하기"} onClick={() => onOpen("share")} />
+                  <Button size="half" color="main" message={"사진 저장"} onClick={() => onOpen("download")} />
+                </div>
+              )}
             </div>
-
-            {mode === "write" ? (
-              <div className="w-full flex flex-col justify-center items-center pb-4">
-                <textarea
-                  name="message"
-                  value={formData.message}
-                  onChange={handleInputChange}
-                  className="w-[280px] mx-auto h-[108px] text-left text-2xl leading-9 flex"
-                  placeholder="편지는 세 줄까지 쓸 수 있어요."
-                  maxLength={MAX_LENTH}
-                />
-                <div className="mt-2 relative text-center border-b border-gray-warm-100">
-                  <span className="text-lg">From. </span>
-                  <input name="name" onChange={handleInputChange} className="text-left w-[130px]" type="text" placeholder="이름은 열 글자까지" maxLength={10} />
-                </div>
-              </div>
-            ) : (
-              <div className="w-full flex flex-col justify-center items-center pb-4">
-                <p className="w-[280px] mx-auto h-[108px] text-left text-2xl leading-9 flex whitespace-pre-line">{formData.message}</p>
-                <div className="mt-2 relative text-center">
-                  <div className="absolute inset-0 pointer-events-none" />
-                  <span className="text-lg relative z-10">From. </span>
-                  <span className="relative z-10">{formData.name}</span>
-                </div>
-              </div>
-            )}
           </div>
-
-          {showCompleteButton ? (
-            <Button size="md" onClick={() => isCompleted && onComplete(formData)} disabled={!isCompleted} message="완성" />
-          ) : (
-            <div className="flex gap-2 justify-center no-capture">
-              <Button size="half" color="main" message={"공유하기"} onClick={() => onOpen("share")} />
-              <Button size="half" color="main" message={"사진 저장"} onClick={() => onOpen("download")} />
+          {mode === "share" && (
+            <div className="absolute right-6 bottom-6 max-h-sm:bottom-2 no-capture">
+              <Button type="replay" shape="circle" color="brand" onClick={() => router.push("/")} />
             </div>
           )}
-        </div>
-      </div>
-      {mode === "share" && (
-        <div className="absolute right-6 bottom-6 max-h-sm:bottom-2 no-capture">
-          <Button type="replay" shape="circle" color="brand" onClick={() => router.push("/")} />
-        </div>
+        </>
       )}
     </Container>
   );
