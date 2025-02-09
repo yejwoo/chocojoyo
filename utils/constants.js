722ce0c88fdee7efd37ba6f108b6e08a73065521
yejwoo <yejinwoo.me@gmail.com>
2025-02-09 16:51:29 +0900

    chore: 사운드 경로 변경

--- utils/constants.js
+++ utils/constants.js
@@ -3,6 +3,8 @@ import tailwindConfig from "@/tailwind.config.mjs";
 export const DOMAIN = "chocojoyo.com"
 export const MAX_HISTORY_LENGTH = 50;
 export const SLOGAN = "나만의 초콜릿으로 달콤한 마음을 전해줘요";
+export const songPath = "/sound/song/"
+export const effectPath = "/sound/effect/"
 export const chocolateColors = tailwindConfig.theme.extend.colors.chocolates;
 export const chocopenColors = tailwindConfig.theme.extend.colors.chocopens;
 export const isTestMode = false;
